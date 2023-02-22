399d953875565860ff0040edf5013ffff6d57a3c
JianHong <leejianhong1@gmail.com>
2023-02-22 14:49:01 +0800

    downgraded consul version

--- frontend/src/config/dev.js
+++ frontend/src/config/dev.js
@@ -1,7 +1,7 @@
 export default {
-  // apmApiUrl: "http://13.229.196.145:3001/api/v1",
+  apmApiUrl: "http://162.55.83.146:3001/api/v1",
   // webSocketEndpoint: "ws://13.229.196.145:3001/api/ws",
-  valkApiUrl: "http://localhost:3001/api/v1",
+  valkApiUrl: "http://localhost:5001/api/v1",
   // monitoringApiUrl: "http://13.229.196.145:8100/api/v1",
   // stakeApiUrl: "https://staging-api.unagii.com/api/v1/tendermint"
 };
