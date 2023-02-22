b93b4009de3fe4b4ae892e32271ec8f76263bfe7
JianHong (Jax) <leejianhong1@gmail.com>
2023-02-22 14:50:41 +0800

    Update dev.js
    added url for apm api

--- frontend/src/config/dev.js
+++ frontend/src/config/dev.js
@@ -1,7 +1,4 @@
 export default {
-  apmApiUrl: "http://162.55.83.146:3001/api/v1",
-  // webSocketEndpoint: "ws://13.229.196.145:3001/api/ws",
-  valkApiUrl: "http://localhost:5001/api/v1",
-  // monitoringApiUrl: "http://13.229.196.145:8100/api/v1",
-  // stakeApiUrl: "https://staging-api.unagii.com/api/v1/tendermint"
+  apmApiUrl: "http://localhost:3001/api/v1",
+  valkApiUrl: "http://localhost:5001/api/v1"
 };
