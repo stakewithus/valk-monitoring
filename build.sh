85f03c4af7cf9ef540decd1838ec787fc744b2f0
root <pohyh@stakewith.us>
2023-03-06 17:52:46 +0100

    increased query to 10s

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-cd apm
-npm install
-npm run build
+ARG=$1
+
+# if [ $ARG == 'frontend' ]
+# then
+#     cd frontend
+#     npm run docker
+# fi
+
+if [ $ARG == 'apm' ]
+then
+    cd apm
+    npm install
+    npm run build
+fi
+
